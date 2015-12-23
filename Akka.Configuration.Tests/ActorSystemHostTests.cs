7d4a031111e646eb9ff261028ea7e311691cc2a3
Philip Laureano <philip.laureano@gmail.com>
2015-12-23 19:55:50 +1100

    Added missing files

---
+++ Akka.Configuration.Tests/ActorSystemHostTests.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Actor;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace Akka.Configuration.Tests
+{
+    [TestFixture]
+    public class ActorSystemHostTests
+    {
+        [Test]
+        public void Should_create_actor_system()
+        {
+            var blockingStrategy = A.Fake<IActorSystemBlockingStrategy>();
+            var builder = A.Fake<IActorSystemBuilder>();
+            var installer = A.Fake<IActorSystemInstaller>();
+            
+            var systemName = "FakeSystem";
+            A.CallTo(() => builder.Create(systemName))
+                .Returns(ActorSystem.Create(systemName));
+
+            var host = new ActorSystemHost(builder, installer, blockingStrategy);
+            host.Run(systemName);
+
+
+            A.CallTo(() => blockingStrategy.AwaitTermination(A<ActorSystem>.Ignored))
+                .MustHaveHappened();
+            A.CallTo(() => builder.Create(systemName)).MustHaveHappened();
+            A.CallTo(() => installer.InstallActors(A<ActorSystem>.Ignored)).MustHaveHappened();
+        }
+    }
+}
---
+++ Akka.Configuration/ActorSystemHost.cs
@@ -0,0 +1,25 @@
+﻿using Akka.Actor;
+
+namespace Akka.Configuration
+{
+    public class ActorSystemHost
+    {
+        private readonly IActorSystemBlockingStrategy _blockingStrategy;
+        private readonly IActorSystemBuilder _builder;
+        private readonly IActorSystemInstaller _installer;
+
+        public ActorSystemHost(IActorSystemBuilder builder, IActorSystemInstaller installer, IActorSystemBlockingStrategy blockingStrategy)
+        {
+            _builder = builder;
+            _installer = installer;
+            _blockingStrategy = blockingStrategy;
+        }
+
+        public void Run(string systemName)
+        {
+            var actorSystem = _builder.Create(systemName);
+            _installer.InstallActors(actorSystem);
+            _blockingStrategy.AwaitTermination(actorSystem);
+        }
+    }
+}
